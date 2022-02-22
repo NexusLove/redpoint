0aee0dc1870af77720d635c1cd3a1de1550b8a83
L5050 <dr.trayaurus61@gmail.com>
2022-02-22 11:24:46 -0800

    official webhook client support

--- client.js
+++ client.js
@@ -8,6 +8,8 @@ import request from "request";
 import { Client as Client, Intents as Intents } from 'discord.js';
 const rl = readline.createInterface({ input, output });
 console.clear();
+let client;
+let webhook;
 let obj = `{
   "token": "your bot token here",
   "webhook": "your webhook URL here"
@@ -22,16 +24,28 @@ process.exit()
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
 let token = config.token
-let webhook = config.webhook
+webhook = config.webhook
 const response = await fetch(webhook)
 const data = await response.json()
 console.log(data)
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS], });
-client.on('message', async message => {
-if (data.channel_id == message.channelId) return;
-console.log(message.channelId)
-console.log(`${message.member.nickname}`)
-})
+client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS], });
+client.on('messageCreate', async message => {
+if (data.channel_id == message.channelId){
+if (message.member) console.log(`${message.member.nickname}: \n${message.content}`);
+}})
 client.login(token);
 }
+async function main(){
+await rl.question('', async (answer) => {
+const hook = new Webhook(webhook);
+hook.send(answer)
+await rl.pause()
+})}
+rl.on('pause', async () => {
+rl.resume()
+await main()
+});
+client.on("ready", async (client) => {
+console.log(`ready`)
+await main()
+})
